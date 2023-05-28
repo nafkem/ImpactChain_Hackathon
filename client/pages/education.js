272eea5c617d40d77d5b424a0a86adfad47b92da
Nofisat Abiodun Ayanlola <50343783+nafkem@users.noreply.github.com>
2023-05-28 21:20:32 +0100

    update impact projects

---
+++ client/pages/education.js
@@ -0,0 +1,88 @@
+export default function Education() { return (
+        <main>
+            <
+    <section id="header-section">
+    <h1 className="text-7xl">Pay a child School fees</h1>
+    <p className="max-w-[400px] text-center">
+        This is a place where first hand informaion reaches you on one click.
+        We give meningful and real information and avoid doctored news or false
+        informaion in order to maintain our integrity and maintain news policy
+        protocol during transmitting or streaming our links.
+    </p>
+    <a href="#"><button>Explore News</button></a>
+</section>
+    <section id="featured-news-section">
+
+    <article class="new-card">
+
+    <a href="#">
+        <img src="./image/pov1.jpg" alt=""/>
+    </a>
+    <small>Featured news</small>
+    <h2>
+        There are more stories on this particular section for you to digest
+        and make out meaningful deductions flow based on reality story.
+    </h2>
+    <p>
+        This is a place where fris hand informaion reaches you on one click.
+        We give meningful and real information and avoid doctored news or false
+        informaion in order to maintain our integrity and maintain news policy
+        protocol during transmitting or streaming our links.
+    </p>
+
+    <a href="#">Read More</a>
+    </article>
+</section>
+<section id="contact area">
+<article class="new-card">
+    
+    <a href="#">
+        <img src="./image/pov.avif" alt=""/>
+    </a>
+    <small>featured news</small>
+    <h2>
+    There are more stories on this particular section for you to digest
+    and make out meaningful deductions flow based on reality story.
+    </h2>
+    <p>
+        This is a place where fris hand informaion reaches you on one click.
+        We give meningful and real information and avoid doctored news or false
+        informaion in order to maintain our integrity and maintain news policy
+        protocol during transmitting or streaming our links.
+    </p>
+    <a href="#">Read More</a>
+</article>
+</section>
+
+            <section id="contact area">
+    <form action="#" method="post">
+        <div class="form input">
+            <label for="">Fullname</label>
+            <br />
+            <input
+                type="text"
+                name=""
+                id=""
+                placeholder="Your Fullname here..."
+                />
+        </div>
+        <div class="form input">
+            <label for="">Email Address</label>
+            <br />
+            <input
+                type="email"
+                name=""
+                id=""
+                placeholder="Your Email Address here..."
+                />
+        </div>
+        <div class="form input">
+            <label for="">Message</label>
+            <br />
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
+            <input type="submit" value="Send Message" />
+            </form>
+            </section>
+        </main>
+    )};
